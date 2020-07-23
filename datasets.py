2397aca3ae9345371eca1cecad0f9348d5c8b25e
Kiran Tomlinson <kt@cs.cornell.edu>
2020-07-23 14:35:19 -0400

    Change to config

--- datasets.py
+++ datasets.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 
 
 DATA_DIR = 'data'
-RESULT_DIR = 'results/triadic-closure-6-standard-feats'
+CONFIG_DIR = 'config'
 
 
 class Dataset(ABC):
@@ -258,7 +258,7 @@ class Dataset(ABC):
 
     @classmethod
     def best_lr(cls, method):
-        with open(f'{RESULT_DIR}/all_grid_search_results.pickle', 'rb') as f:
+        with open(f'{CONFIG_DIR}/learning_rate_settings.pickle', 'rb') as f:
             grid_search_losses, lrs = pickle.load(f)
 
         return lrs[np.argmin([grid_search_losses[cls, method, lr] for lr in lrs])]
